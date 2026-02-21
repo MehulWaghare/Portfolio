08ef3f39cda5d488b146650a46e83f6122d57c11
MehulWaghare <126313845+MehulWaghare@users.noreply.github.com>
2026-02-21 20:58:04 +0530

    Fix manage.py for Render build

--- my_website/manage.py
+++ my_website/manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE',
-    'my_website.my_website.settings'
+    'my_website.settings'
 )
     try:
         from django.core.management import execute_from_command_line
